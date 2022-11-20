49bf3b7cb44c695052fa2606ffb1e50813cedaa6
Shoshick <AVK2003@inbox.ru>
2022-11-20 11:21:18 +0700

    task4 3st commt; changed README.md; changed .gitignore; changed main.c

--- task4/main.c
+++ task4/main.c
@@ -2,36 +2,47 @@
 #include <stdlib.h>
 #include <string.h>
 
-char line_meter[9999];
+#ifndef MAX_BUF
+#define MAX_BUF 999 // USED ​​IN FUNCTION fgets()
+#endif
+char line_meter[MAX_BUF];
+
+
+void freeing_memory(int text, int lines_number)
+{
+  for (int i = 0; i < lines_number; i++)
+    free(text[i]);
+  free(text);
+}
 
 int main() {
-  int len, mas_len = 0;
+  int len, lines_number = 0;
   char **text;
   text = (char **)malloc(1 * sizeof(char *));
   if (text == NULL) {
-    printf("Ошибка, malloc вернул 0");
-    return 0;
+    printf("Ошибка, malloc вернул NULL");
+    freeing_memory(text, lines_number);
+    return 1;
   }
   for (int i = 0;; i++) {
-    fgets(line_meter, 999, stdin);
+    fgets(line_meter, MAX_BUF, stdin);
     if (line_meter[0] == '.') {
       break;
     }
-    mas_len += 1;
+    lines_number += 1;
     text = (char **)realloc(text, i + 1);
     len = strlen(line_meter) + 1; //место для '\0'
     text[i] = (char *)malloc(len * sizeof(char));
     if (text[i] == NULL) {
-      printf("Ошибка, malloc вернул 0");
-      return 0;
+      printf("Ошибка, malloc вернул NULL");
+      freeing_memory(text, lines_number);
+      return 1;
     }
     strcpy(text[i], line_meter);
   }
-  for (int i = 0; i < mas_len; i++) {
-    for (int j = 0; text[i][j] != '\0'; j++) {
-      printf("%c", text[i][j]);
+  for (int i = 0; i < lines_number; i++) {
+      printf("%s", text[i]);
     }
-  }
-  free(text);
+  freeing_memory(text, lines_number);
   return 0;
 }
